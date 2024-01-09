543ac85c352a91a3e87a3de653a6b7274a92ce39
Zeeshan Akhter <zeeshanakhter957@gmail.com>
2024-01-09 19:43:43 +0530

    update: check button

--- src/components/CheckButton.js
+++ src/components/CheckButton.js
@@ -1,11 +1,47 @@
-import React from "react";
+// import React from "react";
+// import axios from "axios";
+// import { toast } from "react-toastify";
+
+// const CheckButton = ({ inputNumber }) => {
+//   const handleCheck = async () => {
+//     try {
+//       if (inputNumber !== null) {
+//         const response = await axios.get(
+//           `https://puremedspa-backend-production.up.railway.app/numbers/check/${inputNumber}`
+//         );
+//         if (
+//           response.data.message === "Number does not exist in the database."
+//         ) {
+//           toast.error(response.data.message);
+//         } else if (response.data.message === "Number exists in the database.") {
+//           toast.success(response.data.message);
+//         }
+//       }
+//     } catch (error) {
+//       toast.error("Error checking data!! ");
+//       console.error("Error checking data:", error);
+//     }
+//   };
+
+//   return (
+//     <button type="button" onClick={handleCheck}>
+//       Check
+//     </button>
+//   );
+// };
+
+// export default CheckButton;
+
+import React, { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const CheckButton = ({ inputNumber }) => {
+const CheckButton = () => {
+  const [inputNumber, setInputNumber] = useState("");
+
   const handleCheck = async () => {
     try {
-      if (inputNumber !== null) {
+      if (inputNumber.trim() !== "") {
         const response = await axios.get(
           `https://puremedspa-backend-production.up.railway.app/numbers/check/${inputNumber}`
         );
@@ -16,17 +52,31 @@ const CheckButton = ({ inputNumber }) => {
         } else if (response.data.message === "Number exists in the database.") {
           toast.success(response.data.message);
         }
+        // setInputNumber("");
       }
     } catch (error) {
-      toast.error("Error checking data!! ");
       console.error("Error checking data:", error);
     }
   };
 
+  const handleInputChange = (e) => {
+    setInputNumber(e.target.value);
+  };
+
   return (
-    <button type="button" onClick={handleCheck}>
-      Check
-    </button>
+    <div>
+      <label htmlFor="numberInput">Number:</label>
+      <input
+        type="number"
+        id="numberInput"
+        value={inputNumber}
+        onChange={handleInputChange}
+        required
+      />
+      <button type="button" onClick={handleCheck}>
+        Check
+      </button>
+    </div>
   );
 };
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,35 @@
-import React, { useState } from "react";
-import InputField from "./InputField";
+// import React, { useState } from "react";
+// import InputField from "./InputField";
+// import CheckButton from "./CheckButton";
+
+// const Form = () => {
+//   const [inputNumber, setInputNumber] = useState(null);
+
+//   return (
+//     <div>
+//       <InputField
+//         setNumber={setInputNumber}
+//         setInputNumberValue={inputNumber}
+//       />
+//       <CheckButton inputNumber={inputNumber} />
+//     </div>
+//   );
+// };
+
+// export default Form;
+
+import React from "react";
 import CheckButton from "./CheckButton";
 
 const Form = () => {
-  const [inputNumber, setInputNumber] = useState(null);
+  // const [apiResponse, setApiResponse] = useState(null);
 
   return (
     <div>
-      <InputField
-        setNumber={setInputNumber}
-        setInputNumberValue={inputNumber}
-      />
-      <CheckButton inputNumber={inputNumber} />
+      <CheckButton />
+      {/* <div className="response-container">
+        {apiResponse && <p>{apiResponse}</p>}
+      </div> */}
     </div>
   );
 };
